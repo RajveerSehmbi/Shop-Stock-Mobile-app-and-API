602ae5e0249c744b471872befe597976d0ef29e2
RajveerSehmbi <rajveer.sehmbi@outlook.com>
2023-11-17 19:08:26 +0000

    server_uri to request_uri

--- api/index.php
+++ api/index.php
@@ -10,7 +10,7 @@ header("Content-type: application/json; charset=UTF-8");
 set_error_handler("ErrorHandler::handleError");
 set_exception_handler("ErrorHandler::handleException");
 
-$parts = explode("/", $_SERVER["SERVER_URI"]);
+$parts = explode("/", $_SERVER["REQUEST_URI"]);
 
 if ($parts[1] === "inventory") {
 
