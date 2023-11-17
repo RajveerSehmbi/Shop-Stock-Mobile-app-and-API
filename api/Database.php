005b90a26a1ba06f37acdaa38ed23302c17249f1
RajveerSehmbi <rajveer.sehmbi@outlook.com>
2023-11-17 19:53:57 +0000

    endpoint id

--- api/Database.php
+++ api/Database.php
@@ -9,7 +9,7 @@ class Database {
                                 private string $password) {}
     public function getConnection(): PDO {
 
-        $dsn = "pgsql:host={$this->host};dbname={$this->name};options=endpoint%3D[ep-winter-violet-50230661-pooler]";
+        $dsn = "pgsql:host={$this->host};dbname={$this->name};options=endpoint%3D[ep-winter-violet-50230661-pooler.us-east-1.postgres.vercel-storage.com]";
 
         return new PDO($dsn, $this->user, $this->password, 
         [PDO::ATTR_EMULATE_PREPARES => false, 
